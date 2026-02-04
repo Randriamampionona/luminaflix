eae12ad7feb740c6cd171dc8fd679450981f37a9
Tojonirina RANDRIAMAMPIONONA <t.randriamampionona.ext@oppycx.com>
2026-02-04 16:39:38 +0100

    FEAT: Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
@@ -75,6 +76,7 @@ export default function RootLayout({
         >
           <Navbar />
           {children}
+          <Analytics />
           <Footer />
         </body>
       </html>
