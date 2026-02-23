1d2222fd0c631ae21f6945273e45dd464786297a
Tojonirina RANDRIAMAMPIONONA <t.randriamampionona.ext@oppycx.com>
2026-02-23 15:46:57 +0100

    FEAT: Update hero slide result

--- action/get-trending-hero.action.ts
+++ action/get-trending-hero.action.ts
@@ -8,17 +8,23 @@ export async function getTrendingHero({
   const API_KEY = process.env.TMDB_API_KEY!;
   const BASE_URL = process.env.BASE_URL!;
 
+  // Generate a random page between 1 and 100
+  const randomPage = Math.floor(Math.random() * 100) + 1;
+
   try {
     const res = await fetch(
-      `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&language=${display_lang || "en-US"}`,
-      { cache: "no-store" },
+      `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${randomPage}&language=${display_lang || "en-US"}`,
+      { cache: "no-store" }, // Ensures a new random page on every request
     );
+
     if (!res.ok) throw new Error("Failed to fetch trending movies");
 
     const data = await res.json();
+
+    // Return the results array
     return data.results;
   } catch (error) {
-    console.error(error);
+    console.error("Lumina_Core_Error:", error);
     return [];
   }
 }
