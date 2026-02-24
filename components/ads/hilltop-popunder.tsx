91493e4391139d1df72a68b689a1a89cc3bebbda
Tojonirina RANDRIAMAMPIONONA <t.randriamampionona.ext@oppycx.com>
2026-02-24 08:51:51 +0100

    REFACTOR: Replace popunder script

--- components/ads/hilltop-popunder.tsx
+++ components/ads/hilltop-popunder.tsx
@@ -1,5 +1,26 @@
 "use client";
 
+import Script from "next/script";
+
 export default function HilltopPopunder() {
-  return <script src="/3dfe1fc676.php"></script>;
+  return (
+    <Script
+      id="hilltop-popunder"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(ltbj){
+            var d = document,
+                s = d.createElement('script'),
+                l = d.scripts[d.scripts.length - 1];
+            s.settings = ltbj || {};
+            s.src = "//creamymouth.com/coD.9W6EbH2/5ilzShWPQB9NNBjBgLy/M/TaM_yANKSV0X2XOkD/ICxfMMzFIF5p";
+            s.async = true;
+            s.referrerPolicy = 'no-referrer-when-downgrade';
+            l.parentNode.insertBefore(s, l);
+          })({})
+        `,
+      }}
+    />
+  );
 }
